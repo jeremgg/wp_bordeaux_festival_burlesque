b60b969f3e3dfb99d33b0b875e9b94891beac774
jeremgg <Votre@email.com>
2016-11-28 22:04:16 +0100

    Adding the template of page reservation

--- page-templates/template-reservation.php
+++ page-templates/template-reservation.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: organisatrices
+ * Template Name: reservation
 */
 
 
@@ -20,96 +20,63 @@ get_header(); ?>
 		</section><!--/.intro-section  -->
 
 
-		<!-- PRESENTATION SECTION -->
-				<section id="about" class="presentation-section">
-		        <div class="container">
+		<!-- BOOKING SECTION -->
+		<section id="about" class="booking-section">
+				<div class="container">
 
 		        <!-- BLOC INTRO -->
 				        <div class="row">
 										<div class="col-lg-12">
-												<h1><?php the_title(); ?></h1>
 												<?php
 														// Contrôler si ACF est actif
 														if ( !function_exists('get_field') ) return;
 												?>
+												<h1><?php the_field('titre_secondaire'); ?></h1>
 												<p><?php the_field('intro'); ?></p>
 										</div>
 				        </div><!-- /.row -->
 
+						<!-- BLOC DE RESERVATION -->
+                <div class="row booking first">
+									<?php $reserv = new WP_Query(array(
+											'post_type' => 'reservations',
+											'posts_per_page' => 4,
+									));  ?>
 
+									<?php while ($reserv->have_posts()) : $reserv->the_post(); ?>
+                    <!-- /.BLOC FIRST -->
+                    <div class="col-xs-6 col-sm-4 col-sm-offset-2 col-md-4 col-md-offset-2 col-lg-4 col-lg-offset-2">
+                        <?php $image = get_field('image'); if (!empty($image)): ?>
+														<img src="<?php echo $image['url']; ?>" class="img-responsive logo" >
+                        <?php endif; ?>
+												<h2><?php the_title(); ?></h2>
+                        <div class="col-sm-4 col-md-4 col-lg-4 links fnac">
+                            <?php $link_1 = get_field('logo_lien_1'); if (!empty($link_1)): ?>
+																<a href="<?php the_field('lien_1'); ?>" target="_blank">
+		                                <img src="<?php echo $link_1['url']; ?>" class="img-responsive logo">
+		                            </a>
+														<?php endif; ?>
+                        </div>
 
-
-		        <!-- BLOC ORGANISATRICES MOBILE-->
-		            <div class="row organisatrices">
-										<?php $organisatrices = new WP_Query(array(
-												'post_type' => 'organisatrices'
-										));  ?>
-
-										<?php while ($organisatrices->have_posts()) : $organisatrices->the_post(); ?>
-												<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 photos">
-														<?php $image = get_field('photo'); if (!empty($image)): ?>
-																<div class="col-xs-12">
-						                        <img class="img-oval" src="<?php echo $image['url']; ?>">
-						                    </div>
-				                    <div class="col-xs-12 col-md-12 col-lg-12">
-				                        <h4><?php the_title(); ?></h4>
-				                        <div class="bg-content">
-																	<p><?php the_content(); ?></p>
-																	<?php $link_1 = get_field('lien_1'); if (!empty($link_1)): ?>
-																			<p><a href="<?php the_field('lien_1'); ?>" target="_blank"><?php the_field('lien_1'); ?></a></p>
-																	<?php endif; ?>
-																	<?php $link_2 = get_field('lien_2'); if (!empty($link_2)): ?>
-																			<p><a href="<?php the_field('lien_2'); ?>" target="_blank"><?php the_field('lien_2'); ?></a></p>
-																	<?php endif; ?>
-																	<?php $link_3 = get_field('lien_3'); if (!empty($link_3)): ?>
-																			<p><a href="<?php the_field('lien_3'); ?>" target="_blank"><?php the_field('lien_3'); ?></a></p>
-																	<?php endif; ?>
-																</div>
-				                    </div>
-														<?php endif ?>
-												</div>
+                        <div class="col-sm-4 col-md-4 col-lg-4 links digitick">
+                            <?php $link_2 = get_field('logo_lien_2'); if (!empty($link_2)): ?>
+																<a href="<?php the_field('lien_2'); ?>" target="_blank">
+		                                <img src="<?php echo $link_2['url']; ?>" class="img-responsive logo">
+		                            </a>
+														<?php endif; ?>
+                        </div>
+                        <div class="col-sm-4 col-md-4 col-lg-4 links ticketmaster">
+                            <?php $link_3 = get_field('logo_lien_3'); if (!empty($link_3)): ?>
+																<a href="<?php the_field('lien_3'); ?>" target="_blank">
+		                                <img src="<?php echo $link_3['url']; ?>" class="img-responsive logo">
+		                            </a>
+														<?php endif; ?>
+                        </div>
+                    </div><!-- /.bloc first -->
 										<?php endwhile; ?>
+                </div><!-- /.row booking first -->
 
-		            </div>
-
-										<?php $organisatrices = new WP_Query(array(
-												'post_type' => 'organisatrices'
-										));  ?>
-
-										<!-- BLOC ORGANISATRICES DESKOP-->
-		                <div class="row photos-deskop">
-												<?php while ($organisatrices->have_posts()) : $organisatrices->the_post(); ?>
-													<?php $image = get_field('photo'); if (!empty($image)): ?>
-				                    <div class="col-sm-4 col-md-4 col-lg-4">
-				                        <img class="img-oval" src="<?php echo $image['url']; ?>">
-				                    </div><!-- /.col-lg-4 -->
-													<?php endif; ?>
-												<?php endwhile; ?>
-		                </div><!-- /.row -->
-
-
-		                <!-- BLOC BIO -->
-		                <div class="row bio-deskop">
-												<?php while ($organisatrices->have_posts()) : $organisatrices->the_post(); ?>
-														<div class="col-sm-4 col-md-4 col-lg-4">
-																<h4><?php the_title(); ?></h4>
-																<div class="bg-content">
-																		<p><?php the_content(); ?></p>
-																		<?php $link_1 = get_field('lien_1'); if (!empty($link_1)): ?>
-																				<p><a href="<?php the_field('lien_1'); ?>" target="_blank"><?php the_field('lien_1'); ?></a></p>
-																		<?php endif; ?>
-																		<?php $link_2 = get_field('lien_2'); if (!empty($link_2)): ?>
-																				<p><a href="<?php the_field('lien_2'); ?>" target="_blank"><?php the_field('lien_2'); ?></a></p>
-																		<?php endif; ?>
-																		<?php $link_3 = get_field('lien_3'); if (!empty($link_3)): ?>
-																				<p><a href="<?php the_field('lien_3'); ?>" target="_blank"><?php the_field('lien_3'); ?></a></p>
-																		<?php endif; ?>
-																</div>
-				                    </div><!-- /.col-lg-4 -->
-												<?php endwhile; ?>
-		                </div><!-- /.row bio -->
-
-		            </div><!-- /.container -->
-		        </section><!-- /.presentation-section -->
+            </div><!-- /.container -->
+        </section><!-- /.booking-section -->
 
 <?php get_footer(); ?>
