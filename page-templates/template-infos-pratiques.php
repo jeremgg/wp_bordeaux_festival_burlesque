7d69ac23e75bfe253eb6f3256d45b32c6e22acf7
jeremgg <Votre@email.com>
2016-11-28 14:00:43 +0100

    Adding the template of page information practices

---
+++ page-templates/template-infos-pratiques.php
@@ -0,0 +1,78 @@
+<?php
+/**
+ * Template Name: infos-pratiques
+*/
+
+
+get_header(); ?>
+
+<!-- INTRO SECTION -->
+		<section id="intro" class="intro-section">
+				<div class="container">
+						<div class="row">
+								<div class="col-lg-12">
+									<div class="entry-content-thumbnail">
+										<?php the_post_thumbnail(); ?>
+									</div>
+								</div>
+						</div>
+				</div>
+		</section><!--/.intro-section  -->
+
+
+		<!-- INFOS PRATIQUES SECTION -->
+				<section id="about" class="infos-section">
+						<div class="container">
+
+								<!-- BLOC ADRESSE -->
+								<div class="row lieu">
+										<?php $adresse = new WP_Query(array(
+												'post_type' => 'lieux'
+										));  ?>
+
+										<?php while ($adresse->have_posts()) : $adresse->the_post(); ?>
+												<div class="col-xs-12 col-sm-4 col-md-4 col-lg-4">
+														<h4><?php the_title(); ?></h4>
+														<p>
+																<?php the_field('adresse'); ?><br>
+																<a href="<?php the_field('site_web'); ?>" target="_blank"><?php the_field('site_web'); ?></a><br>
+																<?php the_field('telephone'); ?>
+														</p>
+												</div><!-- /.col-lg-4 -->
+										<?php endwhile; ?>
+								</div><!-- /.row lieu -->
+
+								<!-- BLOC PRATIQUES -->
+
+							<div class="row pratique">
+									<?php $page = new WP_Query(array(
+											'post_type' => 'pages'
+									)); ?>
+
+									<?php $page->the_post(); ?>
+									<h1><?php the_field('titre_secondaire'); ?></h1>
+
+									<?php $transports = new WP_Query(array(
+											'post_type' => 'transports',
+											'orderby'  => 'order', // ordre par date
+											//'order'  => 'desc', // ordre décendant
+									));  ?>
+									<?php while ($transports->have_posts()) : $transports->the_post(); ?>
+											<div class="row transport">
+													<?php $image = get_field('image'); if (!empty($image)): ?>
+															<div class="col-xs-12 col-sm-5 col-md-5 col-lg-5">
+																	<img class="img-responsive" src="<?php echo $image['url']; ?>" alt="" />
+															</div>
+															<div class="col-xs-12 col-sm-6 col-sm-offset-1 col-md-6 col-md-offset-1 col-lg-6 col-lg-offset-1">
+																	<span>en <?php the_field('vehicule'); ?></span>
+																	<p><?php the_field('adresse'); ?></p>
+															</div>
+													<?php endif ?>
+									</div><!-- row transport -->
+									<?php endwhile; ?>
+							</div><!-- /.row pratique -->
+
+		      </div><!-- /.container -->
+		  </section><!-- /.presentation-section -->
+
+<?php get_footer(); ?>
--- page-templates/template-organisatrices.php
+++ page-templates/template-organisatrices.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Template Name: organisatrices
+ * Template Name: reservation
 */
 
 
@@ -19,97 +19,5 @@ get_header(); ?>
 				</div>
 		</section><!--/.intro-section  -->
 
-
-		<!-- PRESENTATION SECTION -->
-				<section id="about" class="presentation-section">
-		        <div class="container">
-
-		        <!-- BLOC INTRO -->
-				        <div class="row">
-										<div class="col-lg-12">
-												<h1><?php the_title(); ?></h1>
-												<?php
-														// Contrôler si ACF est actif
-														if ( !function_exists('get_field') ) return;
-												?>
-												<p><?php the_field('intro'); ?></p>
-										</div>
-				        </div><!-- /.row -->
-
-
-
-
-		        <!-- BLOC ORGANISATRICES MOBILE-->
-		            <div class="row organisatrices">
-										<?php $organisatrices = new WP_Query(array(
-												'post_type' => 'organisatrices'
-										));  ?>
-
-										<?php while ($organisatrices->have_posts()) : $organisatrices->the_post(); ?>
-												<div class="col-xs-12 col-sm-12 col-md-4 col-lg-4 photos">
-														<?php $image = get_field('photo'); if (!empty($image)): ?>
-																<div class="col-xs-12">
-						                        <img class="img-oval" src="<?php echo $image['url']; ?>">
-						                    </div>
-				                    <div class="col-xs-12 col-md-12 col-lg-12">
-				                        <h4><?php the_title(); ?></h4>
-				                        <div class="bg-content">
-																	<p><?php the_content(); ?></p>
-																	<?php $link_1 = get_field('lien_1'); if (!empty($link_1)): ?>
-																			<p><a href="<?php the_field('lien_1'); ?>" target="_blank"><?php the_field('lien_1'); ?></a></p>
-																	<?php endif; ?>
-																	<?php $link_2 = get_field('lien_2'); if (!empty($link_2)): ?>
-																			<p><a href="<?php the_field('lien_2'); ?>" target="_blank"><?php the_field('lien_2'); ?></a></p>
-																	<?php endif; ?>
-																	<?php $link_3 = get_field('lien_3'); if (!empty($link_3)): ?>
-																			<p><a href="<?php the_field('lien_3'); ?>" target="_blank"><?php the_field('lien_3'); ?></a></p>
-																	<?php endif; ?>
-																</div>
-				                    </div>
-														<?php endif ?>
-												</div>
-										<?php endwhile; ?>
-
-		            </div>
-
-										<?php $organisatrices = new WP_Query(array(
-												'post_type' => 'organisatrices'
-										));  ?>
-
-										<!-- BLOC ORGANISATRICES DESKOP-->
-		                <div class="row photos-deskop">
-												<?php while ($organisatrices->have_posts()) : $organisatrices->the_post(); ?>
-													<?php $image = get_field('photo'); if (!empty($image)): ?>
-				                    <div class="col-sm-4 col-md-4 col-lg-4">
-				                        <img class="img-oval" src="<?php echo $image['url']; ?>">
-				                    </div><!-- /.col-lg-4 -->
-													<?php endif; ?>
-												<?php endwhile; ?>
-		                </div><!-- /.row -->
-
-
-		                <!-- BLOC BIO -->
-		                <div class="row bio-deskop">
-												<?php while ($organisatrices->have_posts()) : $organisatrices->the_post(); ?>
-														<div class="col-sm-4 col-md-4 col-lg-4">
-																<h4><?php the_title(); ?></h4>
-																<div class="bg-content">
-																		<p><?php the_content(); ?></p>
-																		<?php $link_1 = get_field('lien_1'); if (!empty($link_1)): ?>
-																				<p><a href="<?php the_field('lien_1'); ?>" target="_blank"><?php the_field('lien_1'); ?></a></p>
-																		<?php endif; ?>
-																		<?php $link_2 = get_field('lien_2'); if (!empty($link_2)): ?>
-																				<p><a href="<?php the_field('lien_2'); ?>" target="_blank"><?php the_field('lien_2'); ?></a></p>
-																		<?php endif; ?>
-																		<?php $link_3 = get_field('lien_3'); if (!empty($link_3)): ?>
-																				<p><a href="<?php the_field('lien_3'); ?>" target="_blank"><?php the_field('lien_3'); ?></a></p>
-																		<?php endif; ?>
-																</div>
-				                    </div><!-- /.col-lg-4 -->
-												<?php endwhile; ?>
-		                </div><!-- /.row bio -->
-
-		            </div><!-- /.container -->
-		        </section><!-- /.presentation-section -->
 
 <?php get_footer(); ?>
