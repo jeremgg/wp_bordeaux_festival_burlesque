54156bd9f7c696c9e6a16a2695ddf6e22bca38b3
jeremgg <Votre@email.com>
2016-12-04 00:30:03 +0100

    Changing the height of the menu

--- 404.php
+++ 404.php
@@ -11,14 +11,14 @@ get_header(); ?>
 		<section id="intro" class="intro-section content-padder error-404 not-found">
 				<div class="container">
 						<div class="row">
-								<h2 class="page-title"><?php _e( 'Erreur 404, La page demandé n\'existe pas', 'bfb' ); ?></h2>
+								<h2 class="page-title"><?php _e( 'Erreur 404, </br>La page demandé n\'existe pas', 'bfb' ); ?></h2>
 
 								<div class="page-content">
 										<img src="http://localhost:8888/Bordeaux-burlesque-festival/site/wp-content/uploads/2016/12/nippiesBFB6.gif" alt="" class="img-responsive nippies">
 
 										<p><?php _e( 'Mais vous n\'êtes pas venu pour rien !', 'bfb' ); ?></p>
 
-										<?php //get_search_form(); ?>
+										<?php get_search_form(); ?>
 
 								</div><!-- .page-content -->
 						</div><!--/.row  -->
