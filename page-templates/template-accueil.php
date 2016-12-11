d21cbdf7dbe1fa0cd12b543ecf276cb075ca3d78
jeremgg <Votre@email.com>
2016-12-11 19:09:59 +0100

    Center the photo of the slider with the margin

--- page-templates/template-accueil.php
+++ page-templates/template-accueil.php
@@ -24,9 +24,26 @@ get_header(); ?>
 				<section id="home" class="home-section">
 						<div class="container">
 
+							<div class="row">
 
+								<?php while ( have_posts() ) : the_post(); ?>
 
-							<?php echo do_shortcode("[metaslider id=700]"); ?>
+									<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+
+										<div class="entry-content">
+												<?php do_shortcode('[aigpl-gallery-slider]'); ?>
+											<?php the_content(); ?>
+											<?php
+												wp_link_pages( array(
+													'before' => '<div class="page-links">' . __( 'Pages:', '_tk' ),
+													'after'  => '</div>',
+												) );
+											?>
+										</div><!-- .entry-content -->
+										<?php edit_post_link( __( 'Edit', '_tk' ), '<footer class="entry-meta"><span class="edit-link">', '</span></footer>' ); ?>
+									</article><!-- #post-## -->
+						<?php endwhile; // end of the loop. ?>
+							</div>
 
 									<!-- DERNIERS BILLETS DU BLOG -->
 									<div class="row blog">
